51035f9c9606ba3fbc93409893bb47101ed25eca
Maggie Yan <4everyan@gmail.com>
2021-02-06 15:18:18 -0500

    style navbar

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Navbar from './navbar/navbar';
-import Preference from './preference/preference';
-import Portfolio from './portfolio/portfolio';
+// import Preference from './preference/preference';
+// import Portfolio from './portfolio/portfolio';
 
 const App = () => {
   return(
@@ -11,8 +11,8 @@ const App = () => {
           <Navbar />
         </header>
 
-        <Route exact path="/home" component={Preference} />
-        <Route exact path="/calculator" component={Portfolio} />
+        {/* <Route exact path="/home" component={Preference} />
+        <Route exact path="/calculator" component={Portfolio} /> */}
       </div>
   )
 }
