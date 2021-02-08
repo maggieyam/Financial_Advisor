9f1f99bebcad9860a44f5f3b752b6beef8b322df
Maggie Yan <4everyan@gmail.com>
2021-02-08 04:58:17 -0500

    resolving issues

--- src/reducers/portfolio_reducer.js
+++
@@ -1,9 +0,0 @@
-// import { RECEIVE_PREFERENCE } from '../actions/portfolio_actions';
-// import {merge} from 'lodash';
-
-// const portfolioReducer = (state = {}, action) => {
-//   Object.freeze(state);
-//   return null;
-// };
-
-// export default portfolioReducer;
