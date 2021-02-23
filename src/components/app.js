5bfb4c2d7750ef507c2e6bc1e955e45b381f4a5f
Maggie Yan <4everyan@gmail.com>
2021-02-22 23:09:13 -0500

    handle error routing

--- src/components/app.js
+++ src/components/app.js
@@ -5,6 +5,7 @@ import PreferenceContainer from './preferences/preference_container';
 import PortfolioContainer from './portfolio/portfolio_container';
 
 const App = () => {
+ 
   return(
       <div>
         <header>
@@ -13,9 +14,16 @@ const App = () => {
 
         <Route exact path="/" component={PreferenceContainer} /> 
         <Route exact path="/calculator/:level" component={PortfolioContainer} />
+        <Route path="*" component={Page404}/>
       </div>
   )
 }
+
+const Page404 = ({ location }) => (
+   <div>
+      <h2>No match found for <code>{location.pathname}</code></h2>
+   </div>
+);
 
 export default App;
 
